62bb93f21c5b90df240df6b48844a5e003dc3d82
yoga permana tanaya <tanayayoga@gmail.com>
2019-05-29 20:11:16 +0800

    :tada:

---
+++ components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="navbar">
+      <a href="#!">Home</a>
+      <a href="#!">About</a>
+      <a href="#!">Testimonials</a>
+      <a href="#!">Contact Us</a>
+    </div>
+  </div>
+</template>
+
+<style>
+  .navbar a { padding: 12px 16px; text-decoration: none; color:#000; display: flex; justify-content: center; }
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="container">
+    <h1 class="title">About Page</h1>
+  </section>
+</template>
